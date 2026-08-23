ef93eb54752d1d7892499bb9e3ebf254bf79496e
Manuel Henriquez <manuelhm1993@gmail.com>
2026-08-22 21:02:08 -0400

    feat: layouts auth/main y routing con lazy loading (#2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/auth-layout/auth-layout').then((m) => m.AuthLayout),
+    children: [
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./features/auth/auth.routes').then((m) => m.authRoutes),
+      },
+    ],
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layouts/main-layout/main-layout').then((m) => m.MainLayout),
+    children: [
+      {
+        path: 'catalog',
+        loadChildren: () =>
+          import('./features/catalog/catalog.routes').then((m) => m.catalogRoutes),
+      },
+      {
+        path: 'cart',
+        loadChildren: () =>
+          import('./features/cart/cart.routes').then((m) => m.cartRoutes),
+      },
+    ],
+  },
+];
